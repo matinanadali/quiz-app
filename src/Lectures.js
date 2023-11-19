5a73bfb49cc42abb7da190e26f94eb99434fe292
matinanadali <matinanad@gmail.com>
2023-11-19 13:25:50 +0200

    questions added

--- src/Lectures.js
+++ src/Lectures.js
@@ -1,47 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import allQuestions from './data/allQuestions.json' ;
 import allLectures from './data/allLectures.json';
 
 export default function Lectures({onShowQuizClick}) {
-  
-  const [selectedQuestions, setSelectedQuestions] = useState([]);
 
+  const handlePlayClick = (selectedLectureIndex) => {
+    const selectedQuestions = allQuestions[selectedLectureIndex];
+    onShowQuizClick(selectedQuestions);
+  };
   
-    const handlePlayClick = (selectedLectureIndex) => {
-      
-      setSelectedQuestions(allQuestions[selectedLectureIndex]);
-  
-      onShowQuizClick(selectedQuestions);
-    };
-  
-return (
+  return (
   <>
-
-{allLectures ? (
-       (
-        <div className='lectures-section'>
-          {allLectures.map((lecture, i) => (
-            <div className="lecture-box">
-              <h2>{lecture['lecture-title']}</h2>
-              <button
-                disabled={i >= allQuestions.length || allQuestions[i].length === 0}
-                className="lectureButton"
-                onClick={() => handlePlayClick(i)}
-              >
-                Play
-              </button>
-              <button className="lectureButton"><a target="_blank"  rel="noreferrer"  href={lecture['presentation-link']}>See presentation</a></button>
-              <div className='watch-section'>
-              <button className="lectureButton"><a target="_blank"  rel="noreferrer"  href={lecture['lecture-link-1']}>Watch lecture-Part 1</a></button>
-              <button className="lectureButton"><a target="_blank"  rel="noreferrer"  href={lecture['lecture-link-2']}>Watch lecture-Part 2</a></button>
+    <div className='lectures-section'>
+      {allLectures.map((lecture, i) => (
+        <div className="lecture-box">
+          <h2>{lecture['lecture-title']}</h2>
+          <button
+            disabled={i >= allQuestions.length || allQuestions[i].length === 0}
+            className="lectureButton"
+            onClick={() => handlePlayClick(i)}
+          >
+            Play
+          </button>
+          <button className="lectureButton">
+            <a target="_blank"  rel="noreferrer"  href={lecture['presentation-link']}>Go to presentation</a>
+          </button>
+          <div className='watch-section'>
+            <button className="lectureButton">
+              <a target="_blank"  rel="noreferrer"  href={lecture['lecture-link-1']}>Watch lecture-Part 1</a>
+            </button>
+            <button className="lectureButton">
+              <a target="_blank"  rel="noreferrer"  href={lecture['lecture-link-2']}>Watch lecture-Part 2</a>
+            </button>
           </div>
-            </div>
-          ))}
         </div>
-      )
-    ) : (
-      <div>Loading...</div>
-    )}
+      ))}
+    </div>
   </>
-);
+  );
 }
--- src/Quiz.js
+++ src/Quiz.js
@@ -5,31 +5,39 @@ import image3 from "./images/image3.png";
 import image4 from "./images/image4.png";
 import image5 from "./images/image5.png";
 import image6 from "./images/image6.png";
+import image7 from "./images/image7.png";
+import image8 from "./images/image8.png";
+import image9 from "./images/image9.png";
 var r = document.querySelector(':root');
 
 export default function Quiz( {onBackToLecturesClick, questions }) {
+	
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [currentScore, setCurrentScore] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [buttonColors, setButtonColors] = useState(Array(questions[currentQuestion].answerOptions.length).fill("")); 
+	
 	const images = {
 		"image1" : image1,
 		"image2" : image2,
 		"image3" : image3,
 		"image4" : image4,
 		"image5" : image5,
-		"image6" : image6
+		"image6" : image6,
+		"image7" : image7,
+		"image8" : image8,
+		"image9" : image9
 	}
+	
 	function delay(ms) {
 		return new Promise(resolve => setTimeout(resolve, ms));
-	  }
-	 
-	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [currentScore, setCurrentScore] = useState(0);
-	const [showScore, setShowScore] = useState(false);
-
-	const [buttonColors, setButtonColors] = useState(Array(questions[currentQuestion].answerOptions.length).fill("")); // Adjust the array length based on the number of answer options
-
+	}	
+	
 	const handleAnswerClick = (isCorrect, index) => {
 		if (isCorrect) {
 		setCurrentScore(currentScore + 1);
 		}
+		
 		// Update button colors based on correctness
 		const newButtonColors = [...buttonColors];
 		const correctButtonIndex = questions[currentQuestion].answerOptions.findIndex((option) => option.isCorrect === true);
@@ -41,92 +49,102 @@ export default function Quiz( {onBackToLecturesClick, questions }) {
 			newButtonColors[correctButtonIndex] = "#009933";
 			r.style.setProperty('--line-color', '#cc0000');
 		}
+		
 		setButtonColors(newButtonColors);
 		handleNextQuestion();
 	};
-    
+
+	const handleSubmitText = (enteredText, correctText, questionIndex) => {
+		const input = document.getElementById("input" + questionIndex);
+		if (enteredText == correctText) {
+			setCurrentScore(currentScore + 1);
+			input.style.borderColor = "#009933";
+			r.style.setProperty('--line-color', '#009933');
+		} else {
+			const correctAnswer = document.getElementById("correctAnswer" + questionIndex);
+			correctAnswer.style.visibility = "visible";
+			input.style.borderColor =  "#b30000";
+			r.style.setProperty('--line-color', '#cc0000');
+		}
+		handleNextQuestion();
+	}
+
+
 	const handleNextQuestion = async () => {
 		await delay(500);
 		const line = document.getElementsByClassName("bottom-line")[0];
-		line.classList.add("bottom-line-active");
+		line.classList.add("bottom-line-grow");
 		await delay(3000);
 		resetButtonColors();
 		const nextQuestion = currentQuestion + 1;
+		
 		if (nextQuestion < questions.length) {
-			setCurrentQuestion(nextQuestion);
+		  setCurrentQuestion(nextQuestion);
 		} else {
-		setShowScore(true);
+		  setShowScore(true);
 		}
-	}
+	  };
 	
 	const resetButtonColors = () => {
-		// Reset button colors to default (empty string)
 		setButtonColors(Array(questions[currentQuestion].answerOptions.length).fill(""));
 		const line = document.getElementsByClassName("bottom-line")[0];
-		line.classList.remove("bottom-line-active");
+		line.classList.remove("bottom-line-grow");
 	};
 	
- 
-	
 	return (
-		<>
-		  <div className='quiz'>
+		<div className='quiz'>
 			{showScore ? (
-			  <div className='score-section'>
-				<div>	You scored {currentScore} out of {questions.length} </div>
-				<div className='buttonsContainer'>
-				<button className='answerButton' onClick={onBackToLecturesClick}>Back to lectures</button>
-				<button className='answerButton'>Check anwers</button>
-				</div>
-			
-			  </div>
+			  	<div className='score-section'>
+					<div>	You scored {currentScore} out of {questions.length} </div>
+					<div className='buttonsContainer'>
+						<button className='answerButton' onClick={onBackToLecturesClick}>Back to lectures</button>
+					</div>
+			  	</div>
 			) : (
-			  <div className="question-box">
-				<div className='question-section'>
-				  <div className='question-count'>
-					<span>Question {currentQuestion + 1}</span>/{questions.length}
-				  </div>
-				  <div className='question-text'>{questions[currentQuestion].questionText}</div>
-				  
-				  <div>
-				{ 
-				(questions[currentQuestion].questionImage) ?  <img
-				className="questionImage"
-				src={images[questions[currentQuestion].questionImage]}
-				alt={questions[currentQuestion].questionImage}
+			    <div className="question-box">
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					<div> 
+						{ questions[currentQuestion].questionImage ?  
+							<img
+							className="questionImage"
+							src={images[questions[currentQuestion].questionImage]}
+							alt={questions[currentQuestion].questionImage}
+							/> : 
+						<div></div>
+						}
+					</div>
+					</div>
+					<div className='answer-section'> 
+					{ questions[currentQuestion].typeQuestion ? 
+						(<>
+							<form onSubmit={(event)=> event.preventDefault()}>
+						<input placeholder="Type your anwer" id={"input" + currentQuestion}/>
+						<button type="submit" onClick={()=>handleSubmitText(document.getElementById("input" + currentQuestion).value, questions[currentQuestion].typeQuestion, currentQuestion)}>Submit</button>
+						</form>
+						<div style={{visibility : 'hidden'}} className='correctAnswer' id={"correctAnswer" + currentQuestion}>Correct answer: {questions[currentQuestion].typeQuestion} </div></> )
+					:
+						(
+						questions[currentQuestion].answerOptions.map((answerOption, i) => (
+						<button
+						className="answerButton"
+						style={{ borderColor: buttonColors[i] }}
+						onClick={() => {
+							handleAnswerClick(answerOption.isCorrect, i);
+						}}
+						>{answerOption.answerText}
+						</button>
+				  		))
+						)}
+					</div>
 				
-			  /> : <div></div>
-				}
-
-    </div>
-				</div>
-				<div className='answer-section'> 
-					{
-					questions[currentQuestion].typeQuestion ? 
-					(	<form>
-						<input placeholder="Type your anwer" />
-						<button type="submit">Submit</button>
-						</form> )
-					 :
-				
-			(	questions[currentQuestion].answerOptions.map((answerOption, i) => (
-					<button
-					  className="answerButton"
-					  style={{ borderColor: buttonColors[i] }}
-					  onClick={() => {
-						handleAnswerClick(answerOption.isCorrect, i);
-					  }}
-					>
-					  {answerOption.answerText}
-					</button>
-				  )))}
-				</div>
-				
-			  </div>
-			)}
+			    </div>
+				)
+			}
 			<hr className="bottom-line" />
-		  </div> 
-		</>
-	  );
-				 
+		</div> 
+	);			 
 }
