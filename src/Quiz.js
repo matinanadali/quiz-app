7325dad53a573b35f298623719140952e8436d78
matinanadali <matinanad@gmail.com>
2023-11-21 11:09:58 +0200

    added button

--- src/Quiz.js
+++ src/Quiz.js
@@ -117,7 +117,7 @@ export default function Quiz( {onBackToLecturesClick, questions }) {
 			) : (
 			    <div className="question-box">
 					<div className='question-section'>
-					<button className='backtolectures'  onClick={onBackToLecturesClick}><img src={back_icon} className='back-icon' />Back to lectures</button>
+					<button className='backtolectures'  onClick={onBackToLecturesClick}><img alt="" src={back_icon} className='back-icon' />Back to lectures</button>
 						<div className='question-count'>
 							<span>Question {currentQuestion + 1}</span>/{questions.length}
 						</div>
